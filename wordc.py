fea559bc3c2a773d5654ebfa346cd07fe230cae7
Arushi <arushi.a052@gmail.com>
2021-03-27 15:53:16 -0700

    added other stencils

--- wordc.py
+++ wordc.py
@@ -18,12 +18,12 @@ d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 #get user's color preference (black/white or color)
 
 user_topic = 'constitution.txt'
-user_mask = None
+user_mask = 'wolfstencil.jpg'
 color_choice = None #or grey_color_func
 text = open(path.join(d, user_topic)).read()
 
-cloud_mask = np.array(Image.open(path.join(d, "pigstencil.jpg")))
-wordcloud = WordCloud(mask=user_mask, contour_width=3, contour_color='red', max_words=100, min_word_length=4).generate(text)
+cloud_mask = np.array(Image.open(path.join(d, user_mask)))
+wordcloud = WordCloud(mask=cloud_mask, contour_width=3, contour_color='white', max_words=100, min_word_length=4).generate(text)
 #wordcloud.to_file(path.join(d, "pigconstitution.png"))
 
 plt.imshow(wordcloud.recolor(color_func=None), interpolation='bilinear')
