1fdc2d0800c8bdc23ad22fdf59d233efada1b781
Ishika <boilermakerishika@gmail.com>
2021-03-27 15:56:20 -0700

    webserver be poppin

---
+++ webserver.py
@@ -0,0 +1,102 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        #self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        path = str(self.path)
+        path = path[1:]
+        print(path)
+        if (path == "favicon.ico"):
+          self.wfile.write(bytes("<link rel=\"shortcut icon\" href=\"data:image/x-icon;,\" type=\"image/x-icon\">", "utf-8"))
+        else:
+          if(path == ""):
+            path = "index.html"
+          if("html" in path):
+            log = open(path, "r")
+            for line in log:
+              self.wfile.write(bytes(line, "utf-8"))
+          elif ("jpg" in path):
+            log = open(path, "rb")
+            for line in log:
+              self.wfile.write(line)
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+
+
+'''from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import http.server
+import socketserver
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'mywebpage.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+# Create an object of the above class
+handler_object = MyHttpRequestHandler
+
+PORT = 8000
+my_server = socketserver.TCPServer(("", PORT), handler_object)
+
+
+
+# Star the server
+my_server.serve_forever()
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        log = open("index.html", "r")
+        for line in log:
+          self.wfile.write(bytes(line, "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+      webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")'''
