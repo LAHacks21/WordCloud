f422dae939cd6d4b999760a4e7331d9ccc22fb55
Arushi <arushi.a052@gmail.com>
2021-03-27 14:59:50 -0700

    minor updates

--- wordc.py
+++ wordc.py
@@ -9,13 +9,16 @@ import numpy as np
 
 d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 
-text = open(path.join(d, 'constitution.txt')).read()
+#get user's topic choice from frontend
+#get user's image mask choice from frontend
 
-#cloud_mask = np.array(Image.open(path.join(d, "pigstencil.jpg")))
+user_topic = 'constitution.txt'
+user_mask = None
+text = open(path.join(d, user_topic)).read()
 
-wordcloud = WordCloud(mask=None, contour_width=3, contour_color='red', max_words=100, min_word_length=4).generate(text)
-
-wordcloud.to_file(path.join(d, "pigconstitution.png"))
+cloud_mask = np.array(Image.open(path.join(d, "pigstencil.jpg")))
+wordcloud = WordCloud(mask=user_mask, contour_width=3, contour_color='red', max_words=100, min_word_length=4).generate(text)
+#wordcloud.to_file(path.join(d, "pigconstitution.png"))
 
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
