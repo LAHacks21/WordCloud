0bd22c699a9c86d86f04f17fbb08405576f0bf99
Arushi <arushi.a052@gmail.com>
2021-03-27 16:04:33 -0700

    minor changes

--- wordc.py
+++ wordc.py
@@ -16,14 +16,18 @@ d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
 #get user's topic choice from frontend
 #get user's image mask choice from frontend
 #get user's color preference (black/white or color)
+#get user's max words
 
-user_topic = 'constitution.txt'
+#dictionary? 
+
+user_topic = 'strongheart.txt'
 user_mask = 'wolfstencil.jpg'
+user_max = 80
 color_choice = None #or grey_color_func
 text = open(path.join(d, user_topic)).read()
 
 cloud_mask = np.array(Image.open(path.join(d, user_mask)))
-wordcloud = WordCloud(mask=cloud_mask, contour_width=3, contour_color='white', max_words=100, min_word_length=4).generate(text)
+wordcloud = WordCloud(mask=cloud_mask, contour_width=3, contour_color='white', max_words=user_max, min_word_length=4).generate(text)
 #wordcloud.to_file(path.join(d, "pigconstitution.png"))
 
 plt.imshow(wordcloud.recolor(color_func=None), interpolation='bilinear')
