0a6d0e1ee846760ec3588df7cb967e1e99ba205d
Ishika <boilermakerishika@gmail.com>
2021-03-27 17:53:31 -0700

    little changes?

--- webserver.py
+++ webserver.py
@@ -45,6 +45,7 @@ class S(BaseHTTPRequestHandler):
 
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+        #extract the information
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     logging.basicConfig(level=logging.INFO)
